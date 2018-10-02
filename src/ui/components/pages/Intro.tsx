3258e17d25e4cb87fdcf22aff7e16962eb3333be
uiskander <theodsgn@gmail.com>
2018-10-02 16:26:51 +0300

    add-react: add loader.gif

--- src/ui/components/pages/Intro.tsx
+++ src/ui/components/pages/Intro.tsx
@@ -1,8 +1,8 @@
 import * as styles from './styles/intro.styl';
 import * as React from 'react'
-import { BigLogo } from '../head';
-import { Loader } from '../ui';
-import { translate, Trans } from 'react-i18next';
+import {BigLogo} from '../head';
+import {Loader} from '../ui';
+import {translate, Trans} from 'react-i18next';
 
 @translate('extension')
 export class Intro extends React.Component {
@@ -10,8 +10,7 @@ export class Intro extends React.Component {
     render() {
         return <div className={styles.intro}>
             <BigLogo/>
-            <div className={styles.loader}>
-            </div>
+            <div className={styles.loader}></div>
         </div>
     }
 }
