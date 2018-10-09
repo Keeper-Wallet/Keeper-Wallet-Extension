e884eef886cb5c3c751bc8cd73426b238b66a814
vba2000 <vba@mail.ru>
2018-10-09 11:37:27 +0300

    fix date component

--- src/ui/components/ui/Date/FormatDate.tsx
+++ src/ui/components/ui/Date/FormatDate.tsx
@@ -5,16 +5,63 @@ const AddZero = ({ value=0 }) => {
     return <span>{res}</span>
 };
 
-export const DateFormat = ({ value }) => {
-    const date = new Date(value);
-    const day = date.getDate();
-    const month = date.getMonth();
-    const year = date.getFullYear();
-    const minutes = date.getMinutes();
-    const hours = date.getHours();
+const FullYear = ({ value }) => {
+    const year = new Date(value).getFullYear();
+    return <AddZero value={year}/>;
+};
+
+const Month = ({ value }) => {
+    const month = new Date(value).getMonth();
+    return <AddZero value={month}/> ;
+};
+
+const Day = ({ value }) => {
+    const day = new Date(value).getDate();
+    return <AddZero value={day}/>;
+};
+
+
+const Hours = ({ value }) => {
+    const hours = new Date(value).getHours();
+    return <AddZero value={hours}/>;
+};
+
+const Minutes = ({ value }) => {
+    const minutes = new Date(value).getMinutes();
+    return <AddZero value={minutes}/>;
+};
+
+const TYPES = {
+    DD: Day,
+    MM: Month,
+    YYYY: FullYear,
+    hh: Hours,
+    mm: Minutes
+};
+
+const DateFromFormat = ({ value, format }) => {
+    return <span>{
+        Object.keys(TYPES)
+        .map((type) => ({ index: format.indexOf(type), type }))
+        .filter(c => c.index > -1)
+        .sort((a, b) => a.index - b.index)
+        .reduce((prev, current) => {
+            const { index } = current;
+            if (index > prev.index) {
+                const res = format.slice(prev.index, index);
+                prev.index += res.length;
+                prev.data.push(<span key={res}>{res}</span>)
+            }
+            const Comp = TYPES[current.type];
+            prev.data.push(<Comp value={value} key={current.type}></Comp>);
+            prev.index += current.type.length;
+            return prev;
+        }, { data: [], index: 0 }).data
+    }</span>
+};
+
+export const DateFormat = ({ value, format='DD-MM-YYYY hh:mm' }) => {
     return <div>
-        <AddZero value={day}/>-<AddZero value={month}/>-<AddZero value={year}/>
-        <span> </span>
-        <AddZero value={hours}/>:<AddZero value={minutes}/>
+        <DateFromFormat value={value} format={format}/>
     </div>
 };
