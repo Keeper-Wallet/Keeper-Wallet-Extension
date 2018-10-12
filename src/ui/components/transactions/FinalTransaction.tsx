9e9e8ac3bb05da197dbc8aa022b54a563034aa87
vba2000 <vba@mail.ru>
2018-10-12 17:45:50 +0300

    some fixes

--- src/ui/components/transactions/FinalTransaction.tsx
+++ src/ui/components/transactions/FinalTransaction.tsx
@@ -5,7 +5,12 @@ import { Button, BUTTON_TYPE } from '../ui';
 import cn from 'classnames';
 
 const Error = ({ approveError }) => {
-    return <div>ERROR {JSON.stringify(approveError.error, null, 4)}</div>;
+    return <div>
+        <h3>
+            <Trans i18nKey='sign.someError'>Something went wrong</Trans>
+        </h3>
+        <pre>{JSON.stringify(approveError.error, null, 4)}</pre>
+    </div>;
 };
 
 @translate('extension')
@@ -17,6 +22,10 @@ export class FinalTransaction extends React.PureComponent {
         const isApprove = !!transactionStatus.approveOk;
         const isReject = !!transactionStatus.rejectOk;
         const isError = !!transactionStatus.approveError;
+        const message = transactionStatus.approveOk && transactionStatus.approveOk.message || {};
+        const isSend = message.broadcast;
+        const network = message.account && message.account.networkCode;
+        const txLink = `https://${ network === 'T' ? 'testnet' : 'mainet'}.wavesexplorer.com/tx/${message.messageHash}`;
         const className = cn(styles.txBigIcon, 'margin-main', {
             'tx-error-icon': isError,
             'tx-approve-icon': isReject || isApprove,
@@ -28,9 +37,11 @@ export class FinalTransaction extends React.PureComponent {
                 {isReject ? <Trans i18nKey='sign.rejected'>Your transaction is rejected!</Trans> : null}
                 {isError ? <Error approveError={transactionStatus.approveError}/> : null}
             </div>
-            {/*<div className="link">*/}
-                {/*<Trans i18nKey='sign.viewTransaction'>View Transaction</Trans>*/}
-            {/*</div>*/}
+            {isSend ?
+                <a className="link" href={txLink} target="_blank">
+                    <Trans i18nKey='sign.viewTransaction'>View Transaction</Trans>
+                </a> : null
+            }
             <Button type={BUTTON_TYPE.SUBMIT} onClick={this.props.onClick}>
                 <Trans i18nKey='sign.ok'>Okay</Trans>
             </Button>
