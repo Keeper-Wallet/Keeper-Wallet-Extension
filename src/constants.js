fcd27c3d9c8fab099ce4a00be7287902ed207e9c
vba2000 <vba@mail.ru>
2018-12-13 18:32:26 +0300

    Fix lang

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,5 @@
 export const NETWORK_CONFIG = {
-    testnet: {code: 'T', server: 'https://testnet1.wavesnodes.com/', matcher: 'https://matcher.testnet.wavesnodes.com/'},
+    testnet: {code: 'T', server: 'https://pool.testnet.wavesnodes.com/', matcher: 'https://matcher.testnet.wavesnodes.com/'},
     mainnet: {code: 'W', server: 'https://nodes.wavesplatform.com/', matcher: 'https://matcher.wavesplatform.com/'}
 };
 
