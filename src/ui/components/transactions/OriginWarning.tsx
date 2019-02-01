475cb4d9c13532eac7ba951e922f2ed4bb616b6d
vba2000 <vba@mail.ru>
2019-02-01 19:27:39 +0300

    Fix link

--- src/ui/components/transactions/OriginWarning.tsx
+++ src/ui/components/transactions/OriginWarning.tsx
@@ -7,7 +7,7 @@ export const OriginWarning = ({ message }) => {
         return null;
     }
     return <div>
-        <a href={`http://${message.origin}`} target="_blank" className={styles.originAddress}>{message.origin}</a>
+        <div className={styles.originAddress}>{message.origin}</div>
         <div className={styles.originDescription}>
             <Trans i18nKey='transactions.originWarning'>wants to access your Waves Address</Trans>
         </div>
