ed45bca0b44205a3594f0608da6fce6183fe934a
vba2000 <vba@mail.ru>
2019-05-06 14:27:23 +0300

    Fix mass transfer

--- src/ui/components/transactions/Transfer/TransferInfo.tsx
+++ src/ui/components/transactions/Transfer/TransferInfo.tsx
@@ -5,7 +5,7 @@ import { I18N_NAME_SPACE } from '../../../appConfig';
 import { Attachment, Balance, DateFormat } from '../../ui';
 import { getFee, getAmount } from './parseTx';
 import { getMoney } from '../../../utils/converters';
-import { bytesToSafeString } from '../../../utils/waves';
+import { readAttachment } from '../../../utils/waves';
 
 
 @translate(I18N_NAME_SPACE)
@@ -38,7 +38,7 @@ export class TransferInfo extends React.PureComponent<ITransferInfo> {
                 <div className="tx-title tag1 basic500">
                     <Trans i18nKey='transactions.attachment'>Attachment</Trans>
                 </div>
-                <Attachment className={`${styles.txValue} plate fullwidth`} attachment={bytesToSafeString(tx.attachment)}/>
+                <Attachment className={`${styles.txValue} plate fullwidth`} attachment={readAttachment(tx.attachment)}/>
             </div> : null }
             
             <div className={styles.txRow}>
