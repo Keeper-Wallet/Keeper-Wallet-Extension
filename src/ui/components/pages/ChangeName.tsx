b4be0e78e652eda5391d47acae39fe2109bc952e
smelnikov <smelnikov@web3tech.ru>
2021-10-08 21:04:29 +0300

    Account management e2e tests

--- src/ui/components/pages/ChangeName.tsx
+++ src/ui/components/pages/ChangeName.tsx
@@ -11,12 +11,12 @@ class ChangeAccountNameComponent extends React.PureComponent {
     readonly props;
     readonly state = { newName: '', error: false, errors: [] };
     inputEl: Input;
-    
-    getRef = input => this.inputEl = input;
-    setNewNameHandler = event => this.setNewName(event);
+
+    getRef = (input) => (this.inputEl = input);
+    setNewNameHandler = (event) => this.setNewName(event);
     onSubmit = (event) => this.changeName(event);
     blurHandler = () => this.onBlur();
-    
+
     changeName(event) {
         event.preventDefault();
         const name = this.state.newName;
@@ -24,79 +24,85 @@ class ChangeAccountNameComponent extends React.PureComponent {
         this.props.changeAccountName({ name, address });
         this.props.onBack();
     }
-    
+
     setNewName(event) {
         const newName = event.target.value;
         const errors = ChangeAccountNameComponent.validateName(newName, this.props.accounts);
         this.setState({ newName, error: false, errors });
     }
-    
+
     render() {
-        return <div className={styles.content}>
-            <h2 className={`title1 margin3 left`}>
-                <Trans i18nKey='changeName.title'>Change name</Trans>
-            </h2>
-            
-            <div className="tag1 basic500 input-title">
-                <Trans i18nKey='changeName.currentName'>Current account name</Trans>
-            </div>
-            
-            <div className="body1 font400 margin-main-big">{this.props.account.name}</div>
-            
-            <div className="separator margin-main-big"></div>
-            <form onSubmit={this.onSubmit}>
+        return (
+            <div className={styles.content}>
+                <h2 className={`title1 margin3 left`}>
+                    <Trans i18nKey="changeName.title">Change name</Trans>
+                </h2>
+
                 <div className="tag1 basic500 input-title">
-                    <Trans i18nKey='changeName.newName'>New account name</Trans>
+                    <Trans i18nKey="changeName.currentName">Current account name</Trans>
                 </div>
-                
-                <div className="margin-main-big relative">
-                    <Input ref={this.getRef}
-                           onInput={this.setNewNameHandler}
-                           onBlur={this.blurHandler}
-                           error={this.state.error}
-                           value={this.state.newName} maxLength='26'
-                           autoFocus={true}
-                    />
-                    <Error show={this.state.error} errors={this.state.errors}/>
+
+                <div id="currentAccountName" className="body1 font400 margin-main-big">
+                    {this.props.account.name}
                 </div>
-                
-                <Button type='submit' disabled={this.state.errors.length || !this.state.newName}>
-                    <Trans i18nKey='changeName.save'>Save</Trans>
-                </Button>
-            </form>
-        </div>;
+
+                <div className="separator margin-main-big"> </div>
+                <form onSubmit={this.onSubmit}>
+                    <div className="tag1 basic500 input-title">
+                        <Trans i18nKey="changeName.newName">New account name</Trans>
+                    </div>
+
+                    <div className="margin-main-big relative">
+                        <Input
+                            id="newAccountName"
+                            ref={this.getRef}
+                            onInput={this.setNewNameHandler}
+                            onBlur={this.blurHandler}
+                            error={this.state.error}
+                            value={this.state.newName}
+                            maxLength="26"
+                            autoFocus={true}
+                        />
+                        <Error show={this.state.error} errors={this.state.errors} />
+                    </div>
+
+                    <Button id="save" type="submit" disabled={this.state.errors.length || !this.state.newName}>
+                        <Trans i18nKey="changeName.save">Save</Trans>
+                    </Button>
+                </form>
+            </div>
+        );
     }
-    
+
     componentDidMount() {
         //this.inputEl.focus();
     }
-    
+
     onBlur() {
         const errors = ChangeAccountNameComponent.validateName(this.state.newName, this.props.accounts);
         this.setState({ errors, error: errors.length ? errors : null });
     }
-    
+
     static validateName(name: string, accounts) {
         const errors = [];
         const names = accounts.map(({ name }) => name);
-        
+
         if (name.length < CONFIG.NAME_MIN_LENGTH) {
             errors.push({ code: 1, key: 'changeName.errorRequired', msg: 'Required name' });
         }
-        
+
         if (names.includes(name)) {
             errors.push({ code: 2, key: 'changeName.errorInUse', msg: 'Name already exist' });
         }
-        
+
         return errors;
     }
 }
 
-
 const mapToProps = (store) => {
     const activeAccount = store.selectedAccount.address;
     const selected = store.localState.assets.account ? store.localState.assets.account.address : activeAccount;
-    
+
     return {
         account: store.accounts.find(({ address }) => address === selected),
         accounts: store.accounts,
--- tests/AccountManagement.ui.spec.ts
+++ tests/AccountManagement.ui.spec.ts
@@ -1,15 +1,26 @@
 import { App, CreateNewAccount, DEFAULT_PASSWORD } from './utils/actions';
-import { By, until } from 'selenium-webdriver';
+import { By, until, WebElement } from 'selenium-webdriver';
+import { clear } from './utils';
+import { expect } from 'chai';
+
+const ACCOUNT_SWITCH_ANIMATION_DELAY = 600;
+const NOTIFICATION_ANIMATION_DELAY = 600;
+const ACCOUNTS = {
+    RICH: { NAME: 'rich', SEED: 'waves private node seed with waves tokens' },
+    POOR: { NAME: 'poor', SEED: 'waves private node seed without waves tokens' },
+};
 
 describe('Account management', function () {
+    this.timeout(60 * 1000);
+
     before(async function () {
         await App.initVault.call(this, DEFAULT_PASSWORD);
-        await CreateNewAccount.importAccount('rich', 'waves private node seed with waves tokens');
+        await CreateNewAccount.importAccount.call(this, ACCOUNTS.RICH.NAME, ACCOUNTS.RICH.SEED);
 
         await this.driver
             .wait(until.elementLocated(By.xpath("//div[contains(@class, '-assets-addAccount')]")), this.wait)
             .click();
-        await CreateNewAccount.importAccount('poor', 'waves private node seed without waves tokens');
+        await CreateNewAccount.importAccount.call(this, ACCOUNTS.POOR.NAME, ACCOUNTS.POOR.SEED);
     });
 
     after(async function () {
@@ -17,79 +28,381 @@ describe('Account management', function () {
     });
 
     describe('Accounts list', function () {
-        it('Change active account');
+        it('Change active account', async function () {
+            const inactiveAccount = this.driver.wait(
+                until.elementIsVisible(
+                    this.driver.wait(
+                        until.elementLocated(
+                            By.xpath(
+                                "//div[contains(@class, '-assets-assets')]" +
+                                    "//div[contains(@class, 'wallets-list')]" +
+                                    "//div[contains(@class, '-wallet-wallet-')]"
+                            )
+                        ),
+                        this.wait
+                    )
+                ),
+                this.wait
+            );
+            const inactiveAccountName = await inactiveAccount
+                .findElement(By.xpath("//div[contains(@class, '-wallet-accountName')]"))
+                .getText();
+            await inactiveAccount.findElement(By.css('div.inactive-account-icon')).click();
+
+            expect(
+                await this.driver
+                    .wait(
+                        until.elementIsVisible(
+                            this.driver.wait(
+                                until.elementLocated(
+                                    By.xpath(
+                                        "//div[contains(@class, '-assets-assets')]" +
+                                            "//div[contains(@class, '-wallet-activeWallet')]" +
+                                            "//div[contains(@class, '-wallet-accountName')]"
+                                    )
+                                ),
+                                this.wait
+                            )
+                        ),
+                        this.wait
+                    )
+                    .getText()
+            ).to.be.equal(inactiveAccountName);
+        });
+
         it('Updating account balances on import');
+
         it('The balance reflects the leased WAVES');
+
         it('Copying the address of the active account on the accounts screen');
 
         describe('Show QR', function () {
-            it('Opening the screen with the QR code of the address by clicking the "Show QR" button');
+            after(async function () {
+                await this.driver.findElement(By.css('div.arrow-back-icon')).click();
+                await this.driver.sleep(ACCOUNT_SWITCH_ANIMATION_DELAY);
+            });
+
+            it('Opening the screen with the QR code of the address by clicking the "Show QR" button', async function () {
+                await this.driver
+                    .wait(
+                        until.elementLocated(
+                            By.xpath(
+                                "//div[contains(@class, '-assets-assets')]" +
+                                    "//div[contains(@class, '-wallet-activeWallet')]" +
+                                    "//div[contains(@class, 'showQrIcon')]"
+                            )
+                        ),
+                        this.wait
+                    )
+                    .click();
+
+                expect(
+                    await this.driver.wait(
+                        until.elementLocated(By.xpath("//div[contains(@class, '-selectedAccountQr')]")),
+                        this.wait
+                    )
+                ).not.to.be.throw;
+            });
+
             it('Check that QR matches the displayed address');
+
             it('Download QR code'); // file downloaded, filename equals "${address}.png"
         });
     });
 
-    const accountPropertiesShouldBeRight = () => {
-        it('Displayed right "Active"-status icon');
-        it('Go to the address QR code screen');
+    function accountPropertiesShouldBeRight(isActive: boolean) {
+        after(async function () {
+            await this.driver.findElement(By.css('div.arrow-back-icon')).click();
+        });
+
+        it('Displayed right status icon', async function () {
+            const activeStatusCls = isActive ? '-accountInfo-activeAccount' : '-accountInfo-inActiveAccount';
+
+            expect(await this.driver.findElement(By.xpath(`//button[contains(@class, '${activeStatusCls}')]`))).not.to
+                .be.throw;
+        });
+
+        it('Go to the address QR code screen', async function () {
+            await this.driver.findElement(By.css('button.showQrIcon')).click();
+
+            expect(
+                await this.driver.wait(
+                    until.elementLocated(By.xpath("//div[contains(@class, '-selectedAccountQr')]")),
+                    this.wait
+                )
+            ).not.to.be.throw;
+            await this.driver.findElement(By.css('div.arrow-back-icon')).click();
+        });
 
         describe('Address', function () {
-            it('Is displayed');
+            it('Is displayed', async function () {
+                expect(
+                    await this.driver
+                        .findElement(
+                            By.xpath("//div[@id='accountInfoAddress']//div[contains(@class, '-copy-copyTextOverflow')]")
+                        )
+                        .getText()
+                ).matches(/\w+/i);
+            });
+
             it('Copying by clicking the "Copy" button');
         });
 
         describe('Public key', function () {
-            it('Is displayed');
+            it('Is displayed', async function () {
+                expect(
+                    await this.driver
+                        .findElement(
+                            By.xpath(
+                                "//div[@id='accountInfoPublicKey']//div[contains(@class, '-copy-copyTextOverflow')]"
+                            )
+                        )
+                        .getText()
+                ).matches(/\w+/i);
+            });
+
             it('Copying by clicking the "Copy" button');
         });
 
         describe('Private key', function () {
-            it('Is displayed');
+            it('Is hidden', async function () {
+                expect(
+                    await this.driver
+                        .findElement(
+                            By.xpath(
+                                "//div[@id='accountInfoPrivateKey']//div[contains(@class, '-copy-copyTextOverflow')]"
+                            )
+                        )
+                        .getText()
+                ).not.matches(/\w+/i);
+            });
+
             describe('Copying by clicking the "Copy" button', function () {
+                before(async function () {
+                    await this.driver
+                        .findElement(
+                            By.xpath("//div[@id='accountInfoPrivateKey']//div[contains(@class, '-copy-lastIcon')]")
+                        )
+                        .click();
+                });
+
+                it('Clicking "Copy" displays the password entry form', async function () {
+                    expect(
+                        await this.driver.wait(
+                            until.elementLocated(
+                                By.xpath("//form[contains(@class, '-accountInfo-enterPasswordModal')]")
+                            ),
+                            this.wait
+                        )
+                    ).not.to.be.throw;
+                    await this.driver.findElement(By.css('button#passwordCancel')).click();
+                });
+
                 it('Clicking "Cancel" does not copy');
-                it('Copying by clicking the "Copy" button and entering the correct password');
+
+                it('Clicking "Copy" and entering the correct password will copy it');
             });
         });
 
         describe('Backup phrase', function () {
-            it('Is displayed');
+            it('Is hidden', async function () {
+                expect(
+                    await this.driver
+                        .findElement(
+                            By.xpath(
+                                "//div[@id='accountInfoBackupPhrase']//div[contains(@class, '-copy-copyTextOverflow')]"
+                            )
+                        )
+                        .getText()
+                ).not.matches(/\w+/i);
+            });
+
             describe('Copying by clicking the "Copy" button', function () {
+                before(async function () {
+                    await this.driver
+                        .findElement(
+                            By.xpath("//div[@id='accountInfoBackupPhrase']//div[contains(@class, '-copy-lastIcon')]")
+                        )
+                        .click();
+                });
+
+                it('Clicking "Copy" displays the password entry form', async function () {
+                    expect(
+                        await this.driver.wait(
+                            until.elementLocated(
+                                By.xpath("//form[contains(@class, '-accountInfo-enterPasswordModal')]")
+                            ),
+                            this.wait
+                        )
+                    ).not.to.be.throw;
+                    await this.driver.findElement(By.css('button#passwordCancel')).click();
+                });
+
                 it('Clicking "Cancel" does not copy');
-                it('Copying by clicking the "Copy" button and entering the correct password');
+
+                it('Clicking "Copy" and entering the correct password will copy it');
             });
         });
 
-        describe('Rename an account', () => {
-            it('A name that is already in use cannot be specified');
-            it('Unique name specified');
+        describe('Rename an account', function () {
+            let newAccountNameInput: WebElement,
+                newAccountNameErr: WebElement,
+                saveBtn: WebElement,
+                currentAccountName: string,
+                newAccountName: string;
+
+            before(async function () {
+                await this.driver
+                    .wait(
+                        until.elementLocated(By.xpath("//button[contains(@class, '-accountInfo-accountName')]")),
+                        this.wait
+                    )
+                    .click();
+                newAccountNameInput = this.driver.wait(until.elementLocated(By.css('input#newAccountName')), this.wait);
+                newAccountNameErr = this.driver.findElement(
+                    By.xpath("//input[@id='newAccountName']//following-sibling::div[contains(@class, '-error-error')]")
+                );
+                saveBtn = this.driver.findElement(By.css('button#save'));
+                currentAccountName = await this.driver.findElement(By.css('div#currentAccountName')).getText();
+            });
+
+            it('A name that is already in use cannot be specified', async function () {
+                await newAccountNameInput.sendKeys(currentAccountName);
+                await newAccountNameInput.sendKeys('\t');
+                expect(await newAccountNameErr.getText()).matches(/Name already exist/i);
+                expect(await saveBtn.isEnabled()).to.be.false;
+                await clear(newAccountNameInput);
+            });
+
+            it('Unique name specified', async function () {
+                newAccountName = currentAccountName.slice(1);
+                await newAccountNameInput.sendKeys(newAccountName);
+                await newAccountNameInput.sendKeys('\t');
+                expect(await saveBtn.isEnabled()).to.be.true;
+            });
+
+            it('Successfully changed account name', async function () {
+                await saveBtn.click();
+
+                expect(
+                    await this.driver
+                        .wait(until.elementIsVisible(this.driver.findElement(By.css('.modal.notification'))), this.wait)
+                        .getText()
+                ).matches(/Account name changed/i);
+
+                await this.driver.sleep(NOTIFICATION_ANIMATION_DELAY);
+
+                expect(
+                    await this.driver
+                        .wait(
+                            until.elementLocated(
+                                By.xpath("//button[contains(@class, '-accountInfo-accountName')]//span")
+                            ),
+                            this.wait
+                        )
+                        .getText()
+                ).to.be.equal(newAccountName);
+            });
         });
 
-        describe('Delete account', () => {
+        describe('Delete account', function () {
             it('Click "Back" on the account deletion confirmation screen - the account is not deleted');
             it('Click "Delete account" deletes the account');
         });
-    };
+    }
 
     describe('Active account', async function () {
-        it('By clicking on account - go to the account properties screen');
-        await accountPropertiesShouldBeRight.call(this);
+        it('By clicking on account - go to the account properties screen', async function () {
+            await this.driver
+                .wait(
+                    until.elementLocated(
+                        By.xpath(
+                            "//div[contains(@class, '-assets-assets')]" +
+                                "//div[contains(@class, '-wallet-activeWallet')]"
+                        )
+                    ),
+                    this.wait
+                )
+                .click();
+
+            expect(
+                await this.driver.wait(
+                    until.elementLocated(By.xpath("//div[contains(@class, '-accountInfo-content')]")),
+                    this.wait
+                )
+            ).not.to.be.throw;
+        });
+
+        accountPropertiesShouldBeRight.call(this, true);
     });
 
     describe('Inactive account', async function () {
-        it('By clicking on account - go to the account properties screen');
-        await accountPropertiesShouldBeRight.call(this);
+        it('By clicking on account - go to the account properties screen', async function () {
+            await this.driver
+                .wait(
+                    until.elementLocated(
+                        By.xpath(
+                            "//div[contains(@class, '-assets-assets')]" +
+                                "//div[contains(@class, 'wallets-list')]" +
+                                "//div[contains(@class, '-wallet-wallet-')]"
+                        )
+                    ),
+                    this.wait
+                )
+                .click();
+
+            expect(
+                await this.driver.wait(
+                    until.elementLocated(By.xpath("//div[contains(@class, '-accountInfo-content')]")),
+                    this.wait
+                )
+            ).not.to.be.throw;
+        });
+
+        accountPropertiesShouldBeRight.call(this, false);
     });
 
-    describe('Opening a new Wallet tab by clicking the "Wallet" button', () => {
-        it('Mainnet');
+    describe('Opening a new Wallet tab by clicking the "Wallet" button', function () {
+        it('Mainnet', async function () {
+            const currentTab = await this.driver.getWindowHandle();
+            await this.driver.wait(until.elementLocated(By.css('a.transactionsIconBlack')), this.wait).click();
+            const tabs = await this.driver.getAllWindowHandles();
+            expect(tabs).length(2);
+            for (const tab of tabs) {
+                if (tab != currentTab) {
+                    await this.driver.switchTo().window(tab);
+                    await this.driver.close();
+                }
+            }
+            await this.driver.switchTo().window(currentTab);
+        });
+
         it('Testnet');
+
         it('Stagenet');
+
         it('Custom');
     });
-    describe('Opening a new Explorer tab by clicking the "Transactions" button', () => {
-        it('Mainnet');
+
+    describe('Opening a new Explorer tab by clicking the "Transactions" button', function () {
+        it('Mainnet', async function () {
+            const currentTab = await this.driver.getWindowHandle();
+            await this.driver.wait(until.elementLocated(By.css('a.walletIconBlack')), this.wait).click();
+            const tabs = await this.driver.getAllWindowHandles();
+            expect(tabs).length(2);
+            for (const tab of tabs) {
+                if (tab != currentTab) {
+                    await this.driver.switchTo().window(tab);
+                    await this.driver.close();
+                }
+            }
+            await this.driver.switchTo().window(currentTab);
+        });
+
         it('Testnet');
+
         it('Stagenet');
+
         it('Custom');
     });
 });
