d1c303bb2f8e24689e36cfceea48384456c5d62b
smelnikov <smelnikov@web3tech.ru>
2021-10-12 17:21:45 +0300

    Others tests

--- tests/Others.ui.spec.ts
+++ tests/Others.ui.spec.ts
@@ -1,4 +1,29 @@
+import { expect } from 'chai';
+import { By, until } from 'selenium-webdriver';
+import { App } from './utils/actions';
+
 describe('Others', function () {
+    this.timeout(60 * 1000);
+
+    before(async function () {
+        await App.initVault.call(this);
+    });
+
+    it('The current version of the extension is displayed', async function () {
+        const { version } = require('../package');
+
+        expect(
+            await this.driver
+                .wait(
+                    until.elementLocated(
+                        By.xpath("//div[contains(@class, '-bottom-bottom')]//div[contains(@class, 'version')]")
+                    ),
+                    this.wait
+                )
+                .getText()
+        ).matches(new RegExp(version, 'g'));
+    });
+
     it('After signAndPublishTransaction() "View transaction" button leads to the correct Explorer');
 
     it('Signature requests are automatically removed from pending requests after 30 minutes');
