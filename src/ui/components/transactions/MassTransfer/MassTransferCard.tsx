a8c717b355aef9dbee07d5cdeab0128c7e2f8364
smelnikov <smelnikov@web3tech.ru>
2021-10-21 18:33:11 +0300

    MasterTransferCard

--- src/ui/components/transactions/MassTransfer/MassTransferCard.tsx
+++ src/ui/components/transactions/MassTransfer/MassTransferCard.tsx
@@ -3,17 +3,73 @@ import * as React from 'react';
 import { Trans } from 'react-i18next';
 import { TxIcon } from '../TransactionIcon';
 import cn from 'classnames';
-import { Balance } from '../../ui';
+import { Attachment, Balance, Button, BUTTON_TYPE } from '../../ui';
 import { getMoney } from '../../../utils/converters';
-import { getAmount, messageType } from './parseTx';
+import { getAmount, getTransferAmount, messageType } from './parseTx';
+import { readAttachment } from '../../../utils/waves';
+
+const MIN_COUNT = 0;
+
+const Transfers = ({ transfers, totalAmount, count = MIN_COUNT }) => {
+    const assets = { [totalAmount.asset.id]: totalAmount.asset };
+
+    return transfers.slice(0, count).map(({ recipient, amount }) => {
+        const money = getMoney(getTransferAmount(amount, totalAmount.asset.id), assets);
+        return (
+            <div key={recipient} className={styles.txRow}>
+                <div className={cn('body3', 'tx-title-black', styles.massTransferRecipient)}>{recipient}</div>
+                <div className={cn('body3', 'submit400', styles.massTransferAmount)}>
+                    <Balance isShortFormat={true} balance={money} showAsset={false} />
+                </div>
+            </div>
+        );
+    });
+};
+
+const ToggleList = ({ count, currentCount, onClick }) => {
+    const needShowBtn = count > MIN_COUNT;
+    const showAll = !currentCount || currentCount === MIN_COUNT;
+    const newCount = showAll ? count : MIN_COUNT;
+    const toggle = () => onClick(newCount);
+
+    if (!needShowBtn) {
+        return null;
+    }
+
+    return (
+        <div className={styles.toggleList}>
+            <Button onClick={toggle} type={BUTTON_TYPE.TRANSPARENT}>
+                <div className={cn(styles.buttonTextCenter, 'body3')}>
+                    {!showAll ? (
+                        <>
+                            <Trans i18nKey="transactions.transfersClose" />
+                            <i className={styles.arrowUp} />
+                        </>
+                    ) : (
+                        <>
+                            <Trans i18nKey="transactions.transfersShowAll" />
+                            <i className={styles.arrowDown} />
+                        </>
+                    )}
+                </div>
+            </Button>
+        </div>
+    );
+};
 
 export class MassTransferCard extends React.PureComponent<IMassTransfer> {
+    readonly state = Object.create(null);
+
+    toggleShowRecipients = (count) => {
+        this.setState({ count });
+    };
+
     render() {
         const className = cn(styles.massTransferTransactionCard, this.props.className, {
             [styles.massTransferCard_collapsed]: this.props.collapsed,
         });
 
-        const { message, assets } = this.props;
+        const { message, assets, collapsed } = this.props;
         const { data = {} } = message;
         const tx = { type: data.type, ...data.data };
         const amount = getMoney(getAmount(tx), assets);
@@ -26,7 +82,7 @@ export class MassTransferCard extends React.PureComponent<IMassTransfer> {
                     </div>
                     <div>
                         <div className="basic500 body3 margin-min">
-                            <Trans i18nKey="transactions.massTransfer">Mass Send</Trans>
+                            <Trans i18nKey="transactions.massTransfer" />
                         </div>
                         <h1 className="headline1">
                             <Balance
@@ -40,7 +96,42 @@ export class MassTransferCard extends React.PureComponent<IMassTransfer> {
                     </div>
                 </div>
 
-                <div className={styles.cardContent} />
+                <div className={styles.cardContent}>
+                    <div className={styles.expandableList}>
+                        <div className={styles.expandableListHeader}>
+                            <span className={cn(styles.expandableListCounter, 'body3')}>{tx.transfers.length}</span>
+                            <span className={cn(styles.expandableListTitle, 'body3')}>
+                                <Trans i18nKey="transactions.recipients" />
+                            </span>
+                            {!collapsed ? (
+                                <ToggleList
+                                    count={tx.transfers.length}
+                                    currentCount={this.state.count}
+                                    onClick={this.toggleShowRecipients}
+                                />
+                            ) : null}
+                        </div>
+                        {!collapsed ? (
+                            <div
+                                className={cn(styles.expandableListContent, {
+                                    plate: this.state.count,
+                                    'margin-main-top': this.state.count,
+                                })}
+                            >
+                                <Transfers transfers={tx.transfers} totalAmount={amount} count={this.state.count} />
+                            </div>
+                        ) : null}
+                    </div>
+
+                    {tx.attachment && tx.attachment.length ? (
+                        <div className={`${styles.txRow} ${styles.txRowDescription}`}>
+                            <div className="tx-title tag1 basic500">
+                                <Trans i18nKey="transactions.attachment" />
+                            </div>
+                            <Attachment className="plate fullwidth" attachment={readAttachment(tx.attachment)} />
+                        </div>
+                    ) : null}
+                </div>
             </div>
         );
     }
