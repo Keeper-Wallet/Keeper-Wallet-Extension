60ec7805f81783b4c45ead180e4aab21697644d5
smelnikov <smelnikov@users.noreply.github.com>
2021-10-29 19:38:38 +0300

    KEEP-67 Password reset redesign (#134)

--- src/ui/components/pages/ForgotPassword.tsx
+++ src/ui/components/pages/ForgotPassword.tsx
@@ -1,40 +1,76 @@
 import * as styles from './styles/forgotAccount.styl';
 import * as React from 'react';
 import { connect } from 'react-redux';
-import { Trans } from 'react-i18next';
-import { Button, BUTTON_TYPE } from '../ui';
+import { Trans, useTranslation } from 'react-i18next';
+import { Button, Error, Input } from '../ui';
 import { deleteAccount } from '../../actions';
+import cn from 'classnames';
 
-class ForgotPasswordComponent extends React.Component {
-    props;
-    onClickHandler = () => this.props.deleteAccount(null);
-    onBackHandler = () => this.props.onBack();
+function ForgotPasswordComponent({ onBack, deleteAccount }) {
+    const { t } = useTranslation();
+    const [phrase, setPhrase] = React.useState(null);
+    const [isBlur, setBlur] = React.useState(false);
 
-    render() {
-        return (
-            <div className={styles.content}>
-                <h2 className="title1 margin2">
-                    <Trans i18nKey="forgotPassword.attention">Attention!</Trans>
-                </h2>
-                <div className="body1">
-                    <Trans i18nKey="forgotPassword.warn">
-                        If you forget your password, you must restore all your accounts from their SEEDs and set a new
-                        password.
-                    </Trans>
-                </div>
-                <div className={styles.buttonMargin}>
-                    <Button className="margin4" onClick={this.onClickHandler} type="warning">
-                        <Trans i18nKey="forgotPassword.restore">Restore all</Trans>
-                    </Button>
-                </div>
-                <div className={styles.tryAgain}>
-                    <Button type={BUTTON_TYPE.TRANSPARENT} onClick={this.onBackHandler}>
-                        <Trans i18nKey="forgotPassword.tryAgain">Try again</Trans>
-                    </Button>
-                </div>
-            </div>
-        );
+    const defaultPhrase = t('forgotPassword.phrase');
+    const isCorrectLength = phrase?.length >= defaultPhrase.length;
+    const hasError = phrase !== defaultPhrase;
+
+    function handleInput(event) {
+        setPhrase(event.target.value);
+        setBlur(false);
     }
+
+    function handleBlur() {
+        setBlur(false);
+    }
+
+    return (
+        <div className={styles.content}>
+            <i className={`error-icon ${styles.errorIcon}`} />
+
+            <h2 className="title1 margin1">
+                <Trans i18nKey="forgotPassword.attention" />
+            </h2>
+
+            <div className="body1 margin1">
+                <Trans i18nKey="forgotPassword.attentionMessage" />
+            </div>
+
+            <div className={cn('plate', 'body1', 'margin1', styles.error)}>
+                <Trans i18nKey="forgotPassword.warningMessage" />
+            </div>
+            <div className="margin1 margin-main-big-top">
+                <Trans i18nKey="forgotPassword.continueMessage" />
+            </div>
+
+            <div id="defaultPhrase" className="plate center margin1 cant-select">
+                <Trans i18nKey="forgotPassword.phrase" />
+            </div>
+            <div>
+                <Input
+                    autoFocus
+                    id="confirmPhrase"
+                    type="input"
+                    className="margin1"
+                    placeholder="Type here..."
+                    onInput={handleInput}
+                    onBlur={handleBlur}
+                />
+                <Error className={cn('margin1', styles.error)} show={hasError && (isBlur || isCorrectLength)}>
+                    <Trans i18nKey="forgotPassword.phraseError" />
+                </Error>
+            </div>
+
+            <div className="buttons-wrapper">
+                <Button id="resetCancel" onClick={onBack}>
+                    <Trans i18nKey="forgotPassword.resetCancel" />
+                </Button>
+                <Button id="resetConfirm" type="warning" disabled={hasError} onClick={deleteAccount}>
+                    <Trans i18nKey="forgotPassword.resetConfirm" />
+                </Button>
+            </div>
+        </div>
+    );
 }
 
 const actions = {
