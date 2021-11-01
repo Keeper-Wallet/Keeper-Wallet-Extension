5bbe02ea3cf6d7e51c25980c562ad5ef25ad3393
smelnikov <smelnikov@web3tech.ru>
2021-11-01 15:57:17 +0300

    Fixes fee for Reissue, Sponsorship transactions

--- src/controllers/CalculateFeeController.js
+++ src/controllers/CalculateFeeController.js
@@ -1,15 +1,13 @@
 import { DEFAULT_FEE_CONFIG, DEFAULT_FEE_CONFIG_URL } from '../constants';
-import { SIGN_TYPE } from "@waves/signature-adapter";
-import { libs } from "@waves/waves-transactions";
-import { BigNumber } from "@waves/bignumber";
+import { SIGN_TYPE } from '@waves/signature-adapter';
+import { libs } from '@waves/waves-transactions';
+import { BigNumber } from '@waves/bignumber';
 
 const CONFIG_EXPIRATION_TIME = 60 * 60 * 1000;
 
 const FEE_TYPES = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 1002];
 
-
-const promiseCache = (delta, defaultValue) => cb => {
-
+const promiseCache = (delta, defaultValue) => (cb) => {
     const defaultConfig = Promise.resolve(defaultValue);
     let time = null;
     let promise = null;
@@ -27,7 +25,7 @@ const promiseCache = (delta, defaultValue) => cb => {
         }
 
         return await promise;
-    }
+    };
 };
 
 const isAccountHasExtraFee = async (address, node) => {
@@ -47,7 +45,6 @@ const getConfig = async function () {
 
 const getCachingFeeConfig = promiseCache(CONFIG_EXPIRATION_TIME, DEFAULT_FEE_CONFIG)(getConfig);
 
-
 export const calculateFeeFabric = (assetInfoController, networkController) => async (adapter, signData) => {
     const { type } = signData;
 
@@ -60,12 +57,15 @@ export const calculateFeeFabric = (assetInfoController, networkController) => as
     const signable = adapter.makeSignable(signData);
     const address = await adapter.getAddress();
     const assetIds = await signable.getAssetIds();
-    const assets = await Promise.all(assetIds.map(id => assetInfoController.assetInfo(id)));
-    const smartAssets = assets.filter(asset => asset.scripted).map(asset => asset.id);
+    const assets = await Promise.all(assetIds.map((id) => assetInfoController.assetInfo(id)));
+    const smartAssets = assets.filter((asset) => asset.scripted).map((asset) => asset.id);
 
     if (type === SIGN_TYPE.CREATE_ORDER) {
         const minOrderFee = new BigNumber(300000);
-        const matcherAddress = libs.crypto.address({ public: signData.data.matcherPublicKey }, adapter.getNetworkByte());
+        const matcherAddress = libs.crypto.address(
+            { public: signData.data.matcherPublicKey },
+            adapter.getNetworkByte()
+        );
         const extraFee = await isAccountHasExtraFee(matcherAddress, node);
         return signable.getOrderFee(feeConfig, minOrderFee, extraFee, smartAssets);
     }
