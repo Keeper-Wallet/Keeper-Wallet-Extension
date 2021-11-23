1b9ad66112c091039d5139a622f69790f5067a18
Sergey Slipchenko <me@faergeek.online>
2021-11-23 15:21:57 +0300

    Inline src/ui/index.ts into src/ui.js

--- src/ui.js
+++ src/ui.js
@@ -1,10 +1,23 @@
 import 'babel-polyfill';
-import extension from 'extensionizer';
+import './ui/styles/app.styl';
+import './ui/styles/icons.styl';
+import './ui/i18n';
+
 import EventEmitter from 'events';
-import PortStream from './lib/port-stream.js';
-import { cbToPromise, setupDnode, transformMethods } from './lib/dnode-util';
+import extension from 'extensionizer';
 import log from 'loglevel';
-import { initApp } from './ui/index';
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+
+import { cbToPromise, setupDnode, transformMethods } from './lib/dnode-util';
+import PortStream from './lib/port-stream.js';
+import { setLangs } from './ui/actions';
+import { updateState } from './ui/actions/updateState';
+import { Root } from './ui/components/Root';
+import { LANGS } from './ui/i18n';
+import backgroundService from './ui/services/Background';
+import { store } from './ui/store';
 
 const WAVESKEEPER_DEBUG = process.env.NODE_ENV !== 'production';
 log.setDefaultLevel(WAVESKEEPER_DEBUG ? 'debug' : 'warn');
@@ -54,6 +67,24 @@ async function startUi() {
   if (WAVESKEEPER_DEBUG) {
     console.log('closed windows');
   }
-  // Initialize app
-  initApp(background);
+
+  backgroundService.init(background);
+  backgroundService.on(updateState);
+  backgroundService.getNetworks();
+  backgroundService.getState();
+  document.addEventListener('mousemove', () => backgroundService.updateIdle());
+  document.addEventListener('keyup', () => backgroundService.updateIdle());
+  document.addEventListener('mousedown', () => backgroundService.updateIdle());
+  document.addEventListener('focus', () => backgroundService.updateIdle());
 }
+
+store.dispatch(setLangs(LANGS));
+
+ReactDOM.render(
+  <Provider store={store}>
+    <div className="app">
+      <Root />
+    </div>
+  </Provider>,
+  document.getElementById('app-content')
+);
