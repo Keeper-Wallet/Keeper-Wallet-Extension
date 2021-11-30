73f73b2b4090074f3a65a9cd596d400790f8acb6
smelnikov <smelnikov@web3tech.ru>
2021-11-30 13:09:10 +0300

    Close button for `AssetInfo`

--- src/ui/components/pages/assets/assetInfo.tsx
+++ src/ui/components/pages/assets/assetInfo.tsx
@@ -33,6 +33,8 @@ export function AssetInfo({ asset, onCopy, onClose }: Props) {
   return (
     <div className="modal cover">
       <div className="modal-form">
+        <Button className="modal-close" onClick={onClose} type="transparent" />
+
         <div className="margin-main">
           <div className="input-title basic500 tag1">
             <Trans i18nKey="assetInfo.id" />
