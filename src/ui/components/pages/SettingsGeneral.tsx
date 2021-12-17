cbd0b282c131eeb1f35a680093d29e1cc5a30310
smelnikov <smelnikov@web3tech.ru>
2021-12-17 15:21:16 +0300

    Fixed SettingsGeneral menu

--- src/ui/components/pages/SettingsGeneral.tsx
+++ src/ui/components/pages/SettingsGeneral.tsx
@@ -5,6 +5,7 @@ import { Trans } from 'react-i18next';
 import { Button, BUTTON_TYPE, Select } from '../ui';
 import { lock, setIdle, setUiState } from '../../actions';
 import { PAGES } from '../../pageConfig';
+import cn from 'classnames';
 
 class SettingsGeneralComponent extends React.Component {
   readonly props;
@@ -32,50 +33,32 @@ class SettingsGeneralComponent extends React.Component {
           <Trans i18nKey="settings.general">General</Trans>
         </div>
 
-        <div className="margin-main-big">
-          <Select
-            description={
-              <Trans i18nKey="settings.sessionTimeout">
-                Session Timeout in
-              </Trans>
-            }
-            selectList={selectList as any}
-            selected={this.props.idleOptions.type}
-            onSelectItem={this.setIdle}
-          ></Select>
-        </div>
+        <div className={styles.settingsMenu}>
+          <div className="margin-main-big">
+            <Select
+              description={
+                <Trans i18nKey="settings.sessionTimeout">
+                  Session Timeout in
+                </Trans>
+              }
+              selectList={selectList as any}
+              selected={this.props.idleOptions.type}
+              onSelectItem={this.setIdle}
+            />
+          </div>
 
-        {/*<div className={`${styles.settingsMenuItem} ${styles.pairing}`}>
-                    <Button type='transparent'
-                            className={styles.settingsBtn}
-                            onClick={this.pairingHandler}>
-                        <div className='body1 left'>
-                            <Trans i18nKey='settings.pairing'>Device Pairing</Trans>
-                        </div>
-                    </Button>
-                </div>
-
-                <div className={`${styles.settingsMenuItem} ${styles.language}`}>
-                    <Button type='transparent'
-                            className={styles.settingsBtn}
-                            onClick={this.langsHandler}>
-                        <div className='body1 left'>
-                            <Trans i18nKey='settings.langs'>Change language</Trans>
-                        </div>
-                    </Button>
-                </div>*/}
-
-        <div className={`${styles.settingsMenuItem} ${styles.password}`}>
-          <Button
-            id="changePassword"
-            type={BUTTON_TYPE.TRANSPARENT}
-            className={styles.settingsBtn}
-            onClick={this.passwordHandler}
-          >
-            <div className="body1 left">
-              <Trans i18nKey="settings.password">Change password</Trans>
-            </div>
-          </Button>
+          <div className={cn(styles.settingsMenuItem, styles.password)}>
+            <Button
+              id="changePassword"
+              type={BUTTON_TYPE.TRANSPARENT}
+              className={styles.settingsBtn}
+              onClick={this.passwordHandler}
+            >
+              <div className="body1 left">
+                <Trans i18nKey="settings.password">Change password</Trans>
+              </div>
+            </Button>
+          </div>
         </div>
       </div>
     );
