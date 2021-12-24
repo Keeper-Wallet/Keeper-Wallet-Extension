86186c7b73545fb04da61a1d3ead2417f3b0c2a0
smelnikov <smelnikov@web3tech.ru>
2021-12-24 11:27:26 +0300

    View in Explorer in AccountInfo

---
+++ src/ui/urls/index.ts
@@ -0,0 +1,34 @@
+const explorerUrls = new Map([
+  ['W', 'wavesexplorer.com'],
+  ['T', 'testnet.wavesexplorer.com'],
+  ['S', 'stagenet.wavesexplorer.com'],
+  ['custom', 'wavesexplorer.com/custom'],
+]);
+
+export function getAccountLink(networkCode: string, address: string) {
+  const explorer = explorerUrls.get(
+    explorerUrls.has(networkCode) ? networkCode : 'custom'
+  );
+  return `https://${explorer}/address/${address}`;
+}
+
+export function getTxHistoryLink(networkCode: string, address: string): string {
+  return `${getAccountLink(networkCode, address)}/tx/`;
+}
+
+export function getTxDetailLink(networkCode: string, txId: string): string {
+  const explorer = explorerUrls.get(
+    explorerUrls.has(networkCode) ? networkCode : 'custom'
+  );
+  return `https://${explorer}/tx/${txId}`;
+}
+
+export function getAssetDetailLink(
+  networkCode: string,
+  assetId: string
+): string {
+  const explorer = explorerUrls.get(
+    explorerUrls.has(networkCode) ? networkCode : 'custom'
+  );
+  return `https://${explorer}/assets/${assetId}`;
+}
