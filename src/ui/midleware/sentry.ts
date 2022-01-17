a1bdeaa79a7e4b5540ea45a886e0d11f51a9aab3
Sergey Slipchenko <me@faergeek.online>
2022-01-17 13:09:54 +0300

    Don't add duplicate navigation breadcrumbs

--- src/ui/midleware/sentry.ts
+++ src/ui/midleware/sentry.ts
@@ -6,17 +6,23 @@ import { AppState } from 'ui/store';
 export const sentryBreadcrumbs: Middleware<{}, AppState> =
   store => next => action => {
     switch (action.type) {
-      case ACTION.CHANGE_TAB:
-        Sentry.addBreadcrumb({
-          type: 'navigation',
-          category: 'navigation',
-          level: Sentry.Severity.Info,
-          data: {
-            from: store.getState().tab,
-            to: action.payload,
-          },
-        });
+      case ACTION.CHANGE_TAB: {
+        const fromTab = store.getState().tab;
+        const toTab = action.payload;
+
+        if (toTab !== fromTab) {
+          Sentry.addBreadcrumb({
+            type: 'navigation',
+            category: 'navigation',
+            level: Sentry.Severity.Info,
+            data: {
+              from: fromTab,
+              to: toTab,
+            },
+          });
+        }
         break;
+      }
       case ACTION.UPDATE_CURRENT_NETWORK:
         Sentry.setTag('network', action.payload);
 
