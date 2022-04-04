36ac21e28f70a2b6d2b3cbb7e1d1ee4a650ff63a
Sergey Slipchenko <me@faergeek.online>
2022-04-04 12:20:02 +0400

    Use hooks in TxDetailTabs

--- src/ui/components/transactions/BaseTransaction/TxDetailTabs.tsx
+++ src/ui/components/transactions/BaseTransaction/TxDetailTabs.tsx
@@ -1,3 +1,6 @@
+import cn from 'classnames';
+import * as React from 'react';
+import { Trans } from 'react-i18next';
 import {
   Highlight,
   PlateCollapsable,
@@ -6,22 +9,17 @@ import {
   TabPanel,
   TabPanels,
   Tabs,
-} from '../../ui';
-import * as React from 'react';
-import { connect } from 'react-redux';
-import { Trans } from 'react-i18next';
+} from 'ui/components/ui';
+import { useAppSelector } from 'ui/store';
 import * as styles from './txdetailtabs.styl';
-import cn from 'classnames';
 
 interface Props {
-  message: any;
   children?: React.ReactNode;
 }
 
-export const TxDetailTabs = connect(
-  (store: any) => ({ message: store.activePopup?.msg }),
-  null
-)(function TxDetailTabs({ message, children }: Props) {
+export function TxDetailTabs({ children }: Props) {
+  const message = useAppSelector(state => state.activePopup?.msg);
+
   return (
     <Tabs>
       <TabList className="body3">
@@ -45,4 +43,4 @@ export const TxDetailTabs = connect(
       </TabPanels>
     </Tabs>
   );
-});
+}
