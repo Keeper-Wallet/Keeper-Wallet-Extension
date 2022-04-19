0e4ddf04a9961302d9dc300f0fef40e417b11bd1
Sergey Slipchenko <me@faergeek.online>
2022-04-19 11:32:15 +0300

    Move account info into sticky header

--- src/ui/components/pages/swap/result.tsx
+++ src/ui/components/pages/swap/result.tsx
@@ -6,7 +6,6 @@ import { Trans } from 'react-i18next';
 import { Button } from 'ui/components/ui/buttons/Button';
 import { Balance } from 'ui/components/ui/balance/Balance';
 import { useAppSelector } from 'ui/store';
-import { SwapAccountInfoHeader } from './accountInfoHeader';
 import { SwapLayout } from './layout';
 import * as styles from './result.module.css';
 
@@ -158,10 +157,6 @@ export function SwapResult({ fromMoney, transactionId, onClose }: Props) {
   return (
     <SwapLayout>
       <div className={styles.root}>
-        <div className={styles.accountInfoHeader}>
-          <SwapAccountInfoHeader account={selectedAccount} />
-        </div>
-
         <div className={styles.content}>
           <div className={styles.statusBox}>
             {swapStatus === SwapStatus.Pending ? (
