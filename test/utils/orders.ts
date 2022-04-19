cb88707b5a5b9ec9c4957f3f1413773ea217f78d
Sergey Slipchenko <me@faergeek.online>
2022-04-19 14:24:13 +0300

    Add price precision conversion for create order

--- test/utils/orders.ts
+++ test/utils/orders.ts
@@ -19,6 +19,27 @@ export const CREATE_ORDER = {
   },
 };
 
+export const CREATE_ORDER_WITH_PRICE_PRECISION_CONVERSION = {
+  type: 1002,
+  data: {
+    matcherPublicKey: '8QUAqtTckM5B8gvcuP7mMswat9SjKUuafJMusEoSn1Gy',
+    orderType: 'buy',
+    expiration: Date.now() + 100000,
+    amount: {
+      tokens: '1.000000',
+      assetId: '5Sh9KghfkZyhjwuodovDhB6PghDUGBHiAPZ4MkrPgKtX',
+    },
+    price: {
+      tokens: '1.014002',
+      assetId: '25FEqEjRkqK6yCkiT7Lz6SAYz7gUFCtxfCChnrVFD5AT',
+    },
+    matcherFee: {
+      tokens: '0.04077612',
+      assetId: 'EMAMLxDnv3xiz8RXg8Btj33jcEw3wLczL3JKYYmuubpc',
+    },
+  },
+};
+
 export const CANCEL_ORDER = {
   type: 1003,
   data: {
