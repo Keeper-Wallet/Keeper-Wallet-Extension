6b17c6bdb2aee03a9bad802d6ede4da0854dd582
smelnikov <smelnikov@web3tech.ru>
2022-05-11 10:05:19 +0400

    `*.styl` to `*.module.css`

--- src/ui/components/bottom/Bottom.tsx
+++ src/ui/components/bottom/Bottom.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import * as styles from './bottom.styl';
+import * as styles from './bottom.module.css';
 import cn from 'classnames';
 import { Network } from './components';
 import { useAppSelector } from 'ui/store';
