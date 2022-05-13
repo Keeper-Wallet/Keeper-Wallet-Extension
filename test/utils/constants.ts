7c76c6fd45591e47b482dea821086513f0063920
ehassed3 <ehassed3@gmail.com>
2022-05-13 11:55:52 +0300

    Change default delay

--- test/utils/constants.ts
+++ test/utils/constants.ts
@@ -14,8 +14,8 @@ export const CUSTOMLIST = ['docs.waves.tech', 'waves.tech'];
 
 export const DEFAULT_PASSWORD = 'default-password';
 
-export const DEFAULT_ANIMATION_DELAY = 600;
-export const DEFAULT_PAGE_LOAD_DELAY = 600;
+export const DEFAULT_ANIMATION_DELAY = 800;
+export const DEFAULT_PAGE_LOAD_DELAY = 800;
 export const DEFAULT_SWITCH_NETWORK_DELAY = 250;
 export const SERVICE_WORKER_INSTALLATION_DELAY = 4000;
 export const SEND_UPDATE_DEBOUNCE_DELAY = 600;
