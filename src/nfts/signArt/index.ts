d549233804f85d13bcc51d10d9404ea5aa60d557
smelnikov <smelnikov@web3tech.ru>
2022-07-12 17:47:49 +0400

    deal with sign-art while info is fetching

--- src/nfts/signArt/index.ts
+++ src/nfts/signArt/index.ts
@@ -12,33 +12,47 @@ export interface SignArtInfo extends BaseInfo {
 
 export class SignArt extends BaseNft<SignArtInfo> {
   get creator(): string {
+    if (!this.info) {
+      const match = super.description?.match(/creator: (\w+)/i);
+      return match && match[1];
+    }
+
     return this.info.creator;
   }
 
   get displayCreator(): string {
-    return this.info.userName ? `@${this.info.userName}` : super.displayCreator;
+    return this.info?.userName ? `@${this.info.userName}` : this.creator;
   }
 
   get creatorUrl(): string {
-    return `https://mainnet.sign-art.app/user/${this.creator}`;
+    return this.creator && `https://mainnet.sign-art.app/user/${this.creator}`;
   }
 
   get marketplaceUrl(): string {
-    return `https://mainnet.sign-art.app/user/${this.creator}/artwork/${this.info.artworkId}`;
+    let artworkId = this.info?.artworkId;
+
+    if (!this.info) {
+      const match = super.description?.match(/artid: (\w+)/i);
+      artworkId = match && match[1];
+    }
+
+    return (
+      this.creator &&
+      artworkId &&
+      `https://mainnet.sign-art.app/user/${this.creator}/artwork/${artworkId}`
+    );
   }
 
   get displayName(): string {
-    return this.info.name || super.displayName;
+    return this.info?.name ?? super.displayName;
   }
 
   get description(): string {
-    return this.info.description || super.description;
+    return this.info?.description ?? super.description;
   }
 
   get foreground(): string {
-    if (!this.info.cid) {
-      return null;
-    }
+    if (!this.info?.cid) return null;
 
     const [domain, filename] = this.info.cid.split('/');
 
