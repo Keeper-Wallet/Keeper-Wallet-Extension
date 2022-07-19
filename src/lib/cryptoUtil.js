5a33026722dc2de61dd21c6b73461939e8bd7e10
Sergey Slipchenko <me@faergeek.online>
2022-07-19 17:42:43 +0300

    lib/cryptoUtil.js -> lib/cryptoUtil.ts

--- src/lib/cryptoUtil.js
+++
@@ -1,6 +0,0 @@
-import { libs } from '@waves/waves-transactions';
-
-export function networkByteFromAddress(address) {
-  const rawNetworkByte = libs.crypto.base58Decode(address).slice(1, 2);
-  return String.fromCharCode(rawNetworkByte);
-}
