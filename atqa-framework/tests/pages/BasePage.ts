d565b55a266fd0f40d9e92b4aa2d14d25fc53d08
Denis <nostromodev@gmail.com>
2022-08-01 00:00:02 +0300

    init framework commit

---
+++ atqa-framework/tests/pages/BasePage.ts
@@ -0,0 +1,117 @@
+import { ClockUnit } from '../../utils/clockUnit';
+import { Locator } from '../../interfaces/Locator.interface';
+
+const clockUnit = new ClockUnit();
+const { I } = inject();
+
+export class BasePage {
+  public BROWSER_URLS = {
+    CHROMIUM: (id: string) => ({
+      POPUP_URI: `chrome-extension://${id}/popup.html`,
+    }),
+    GECKO: (id: string) => ({
+      POPUP_URI: `moz-extension://${id}/popup.html`,
+    }),
+    CHROME_SYSTEM: 'chrome://system/',
+    CHROMIUM_EXTENSIONS_PAGE: 'chrome://extensions/',
+    FIREFOX_EXTENSIONS_PAGE: 'about:debugging#/runtime/this-firefox',
+  };
+
+  public BROWSER_SELECTORS = {
+    CHROMIUM_EXTENSION_VALUE_BUTTON: { id: 'extensions-value-btn' },
+    CHROMIUM_EXTENSIONS_LIST: { xpath: '(//div[@class="stat-value"])[5]' },
+    CHROMIUM: {
+      DETAILS_BUTTON: {
+        shadow: [
+          'extensions-manager',
+          'extensions-item-list',
+          'extensions-item',
+          '#detailsButton',
+        ],
+      },
+      DEV_MODE_TOGGLE: {
+        shadow: ['extensions-manager', 'extensions-toolbar', '#devMode'],
+      },
+    },
+    EDGE: {
+      DETAILS_BUTTON: { xpath: '//span[contains(text(),"Details")]' },
+      DEV_MODE_TOGGLE: { css: '#developer-mode' },
+      EXTENSION_MENU_BUTTON: {
+        xpath: '(//button[@aria-label= "Extensions menu"])[2]',
+      },
+    },
+    FIREFOX: {
+      MANIFEST_EMAIL_TEXT: 'support@wavesplatform.com',
+    },
+  };
+
+  //TODO: Make it workable and init as main method for getId (now the elements on the system page can't be found for some reason)
+  public getChromeExtensionId = async (): Promise<string> => {
+    I.amOnPage(this.BROWSER_URLS.CHROME_SYSTEM);
+    I.seeElement({
+      xpath: '//*[@id="extensions-value"]',
+    });
+    I.wait(clockUnit.SECONDS * 10);
+    const extList = await I.grabTextFrom({
+      xpath: '//*[@id="extensions-value"]',
+    });
+    console.log(extList);
+    let goo = '';
+    let [id, name] = '';
+    for (goo of extList.split('\n')) {
+      [id, name] = goo.split(' : ');
+      if (name.toLowerCase() === 'Keeper Wallet'.toLowerCase()) {
+        console.log(id, 'EXT ID');
+      }
+    }
+    return id;
+  };
+
+  public getExtensionId = async (element: Locator): Promise<string> => {
+    const devModeSelector: Locator =
+      this.BROWSER_SELECTORS.CHROMIUM.DEV_MODE_TOGGLE;
+    const detailButtonSelector: Locator =
+      this.BROWSER_SELECTORS.CHROMIUM.DETAILS_BUTTON;
+    const edgeDevModeSelector: Locator =
+      this.BROWSER_SELECTORS.EDGE.DEV_MODE_TOGGLE;
+    const edgeDetailsButton: Locator =
+      this.BROWSER_SELECTORS.EDGE.DETAILS_BUTTON;
+    const chromiumExtensionPage: Locator =
+      this.BROWSER_URLS.CHROMIUM_EXTENSIONS_PAGE;
+    const firefoxExtensionPage: Locator =
+      this.BROWSER_URLS.FIREFOX_EXTENSIONS_PAGE;
+    const firefoxManifestEmail: Locator =
+      this.BROWSER_SELECTORS.FIREFOX.MANIFEST_EMAIL_TEXT;
+    switch (process.env.BROWSER_INIT_NAME) {
+      case 'opera':
+        I.amOnPage(chromiumExtensionPage);
+        I.waitForElement(devModeSelector);
+        I.click(devModeSelector);
+
+        I.waitForElement(detailButtonSelector);
+        I.click(detailButtonSelector);
+        break;
+      case 'chrome':
+        I.amOnPage(chromiumExtensionPage);
+        I.waitForElement(devModeSelector);
+        I.click(devModeSelector);
+
+        I.waitForElement(detailButtonSelector);
+        I.click(detailButtonSelector);
+        break;
+      case 'MicrosoftEdge':
+        I.amOnPage(chromiumExtensionPage);
+        I.waitForElement(edgeDevModeSelector);
+        I.click(edgeDevModeSelector);
+
+        I.waitForElement(edgeDetailsButton);
+        I.click(edgeDetailsButton);
+        break;
+      case 'firefox':
+        I.amOnPage(firefoxExtensionPage);
+        I.waitForText(firefoxManifestEmail, clockUnit.SECONDS * 30);
+    }
+    I.waitForElement(element);
+    return await I.grabTextFrom(element);
+  };
+}
---
+++ atqa-framework/utils/ExtensionInitHandler.ts
@@ -0,0 +1,59 @@
+import { BasePage } from '../tests/pages/BasePage';
+import { ExtensionInitPage } from '../tests/pages/ExtensionInitPage';
+import { Locator } from '../interfaces/Locator.interface';
+import fs from 'fs-extra';
+import path from 'path';
+
+const extensionInitPage = new ExtensionInitPage();
+const basePage = new BasePage();
+
+const { I } = inject();
+const args = process.env.BROWSER_INIT_NAME;
+
+export class ExtensionInitHandler {
+  public installAddOnHelper = async (path: string): Promise<void> => {
+    I.useWebDriverTo('Install Gecko AddOn', async ({ browser }) => {
+      const extension = await fs.readFile(path);
+      browser.installAddOn(extension.toString('base64'), true);
+    });
+  };
+
+  public extensionInit = async (): Promise<void> => {
+    const chromiumExtensionId: Locator =
+      extensionInitPage.SELECTORS.CHROMIUM.EXTENSION_ID;
+    const edgeExtensionId: Locator =
+      extensionInitPage.SELECTORS.EDGE.EXTENSION_ID;
+    const firefoxExtensionId: Locator =
+      extensionInitPage.SELECTORS.FIREFOX.EXTENSION_ID;
+    switch (args) {
+      case 'chrome': {
+        const extId = await basePage.getExtensionId(chromiumExtensionId);
+        I.amOnPage(basePage.BROWSER_URLS.CHROMIUM(extId).POPUP_URI);
+        break;
+      }
+      case 'opera': {
+        const extId = await basePage.getExtensionId(chromiumExtensionId);
+        I.amOnPage(basePage.BROWSER_URLS.CHROMIUM(extId).POPUP_URI);
+        break;
+      }
+      case 'MicrosoftEdge': {
+        const extId = await basePage.getExtensionId(edgeExtensionId);
+        I.amOnPage(basePage.BROWSER_URLS.CHROMIUM(extId).POPUP_URI);
+        break;
+      }
+      case 'firefox': {
+        await this.installAddOnHelper(
+          path.join(
+            __dirname,
+            '..',
+            '..',
+            'dist',
+            'keeper-wallet-2.9.0-firefox.xpi'
+          )
+        );
+        const extId = await basePage.getExtensionId(firefoxExtensionId);
+        I.amOnPage(basePage.BROWSER_URLS.GECKO(extId).POPUP_URI);
+      }
+    }
+  };
+}
