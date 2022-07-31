8a9a80d6d51d74b024215114f8a07540b6278a75
Denis <nostromodev@gmail.com>
2022-08-01 00:00:02 +0300

    init framework commit

---
+++ atqa-framework/interfaces/Locator.interface.ts
@@ -0,0 +1,1 @@
+export type Locator = CodeceptJS.LocatorOrString;
---
+++ atqa-framework/tests/integration/scenario.Test.ts
@@ -0,0 +1,67 @@
+import { ClockUnit } from '../../utils/clockUnit';
+import { BasePage } from '../pages/BasePage';
+import { ExtensionInitHandler } from '../../utils/ExtensionInitHandler';
+import { AccountPage } from '../pages/AccountPage';
+
+const basePage = new BasePage();
+const clockUnit = new ClockUnit();
+const extensionInitHandler = new ExtensionInitHandler();
+const accountPage = new AccountPage();
+
+const { I } = inject();
+
+Feature('Initial Test');
+
+Background(async () => {
+  return extensionInitHandler.extensionInit();
+});
+
+Scenario('Init scenario', async () => {
+  I.waitForClickable(
+    accountPage.SELECTORS.GET_STARTED_BUTTON,
+    clockUnit.SECONDS * 30
+  );
+  I.wait(clockUnit.SECONDS * 10);
+  I.click(accountPage.SELECTORS.GET_STARTED_BUTTON);
+  I.waitForElement(
+    accountPage.SELECTORS.CREATE_PASSWORD_INPUT.CREATE_PASSWORD,
+    clockUnit.SECONDS * 30
+  );
+  I.fillField(
+    accountPage.SELECTORS.CREATE_PASSWORD_INPUT.CREATE_PASSWORD,
+    '12345678'
+  );
+  I.waitForElement(
+    accountPage.SELECTORS.CREATE_PASSWORD_INPUT.CONFIRM_PASSWORD
+  );
+  I.fillField(
+    accountPage.SELECTORS.CREATE_PASSWORD_INPUT.CONFIRM_PASSWORD,
+    '12345678'
+  );
+  I.waitForElement(
+    accountPage.SELECTORS.CREATE_PASSWORD_INPUT.ACCEPT_TERMS_CHECKBOX,
+    clockUnit.SECONDS * 30
+  );
+  I.click(accountPage.SELECTORS.CREATE_PASSWORD_INPUT.ACCEPT_TERMS_CHECKBOX);
+  I.waitForElement(
+    accountPage.SELECTORS.CREATE_PASSWORD_INPUT.ACCEPT_CONDITIONS,
+    clockUnit.SECONDS * 30
+  );
+  I.click(accountPage.SELECTORS.CREATE_PASSWORD_INPUT.ACCEPT_CONDITIONS);
+  I.waitForElement(
+    accountPage.SELECTORS.CREATE_PASSWORD_INPUT.CONTINUE_BUTTON,
+    clockUnit.SECONDS * 30
+  );
+  I.click(accountPage.SELECTORS.CREATE_PASSWORD_INPUT.CONTINUE_BUTTON);
+  I.waitForElement(
+    accountPage.SELECTORS.ACCOUNTS.ADD_ACCOUNT_BUTTON,
+    clockUnit.SECONDS * 30
+  );
+  I.click(accountPage.SELECTORS.ACCOUNTS.ADD_ACCOUNT_BUTTON);
+  I.waitForElement(
+    accountPage.SELECTORS.ACCOUNTS.ADD_KEYSTORE_BUTTON,
+    clockUnit.SECONDS * 30
+  );
+  I.click(accountPage.SELECTORS.ACCOUNTS.ADD_KEYSTORE_BUTTON);
+  I.wait(clockUnit.MINUTES * 2);
+});
