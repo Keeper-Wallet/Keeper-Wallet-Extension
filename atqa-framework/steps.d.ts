683bdcebf98f05472f8a505ab25f9525dc583cff
Denis <nostromodev@gmail.com>
2022-08-01 12:12:24 +0300

    fix prettier alerts

--- atqa-framework/steps.d.ts
+++ atqa-framework/steps.d.ts
@@ -2,10 +2,12 @@
 /// <reference types='codeceptjs' />
 
 declare namespace CodeceptJS {
-    interface SupportObject { I: I }
-    interface Methods extends WebDriver {}
-    interface I extends WithTranslation<Methods> {}
-    namespace Translation {
-        interface Actions {}
-    }
+  interface SupportObject {
+    I: I;
+  }
+  interface Methods extends WebDriver {}
+  interface I extends WithTranslation<Methods> {}
+  namespace Translation {
+    interface Actions {}
+  }
 }
