44c97454a1f126555d608afa38a50a9ddc9e23c2
Denis <nostromodev@gmail.com>
2022-08-22 20:36:32 +0300

    remove redundant async

--- atqa-framework/tests/e2e/steps/BaseSteps.ts
+++ atqa-framework/tests/e2e/steps/BaseSteps.ts
@@ -3,7 +3,7 @@ import { ClockUnit } from '../../../utils/clockUnit';
 const clockUnit = new ClockUnit();
 const { I } = inject();
 
-When('DEBUG PAUSE', async () => {
+When('DEBUG PAUSE', () => {
   I.wait(clockUnit.MINUTES * 60);
   console.info('=======DEBUG PAUSE=======');
 });
