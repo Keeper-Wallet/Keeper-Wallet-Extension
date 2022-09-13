a649b6ba7318d19f5431e06d7b508d124f4eede0
Sergey Slipchenko <faergeek@ya.ru>
2022-09-13 13:43:53 +0300

    Turn SettingsGeneral into a functional component

--- src/ui/components/pages/SettingsGeneral.tsx
+++ src/ui/components/pages/SettingsGeneral.tsx
@@ -1,86 +1,63 @@
 import * as styles from './styles/settings.styl';
 import * as React from 'react';
-import { connect } from 'react-redux';
-import { WithTranslation, withTranslation } from 'react-i18next';
+import { useTranslation } from 'react-i18next';
 import { Button, Select } from '../ui';
-import { lock, setIdle, setUiState } from '../../actions';
+import { setIdle } from '../../actions';
 import { PageComponentProps, PAGES } from '../../pageConfig';
 import cn from 'classnames';
-import { AppState } from 'ui/store';
+import { useAppDispatch, useAppSelector } from 'ui/store';
 
-interface StateProps {
-  autoClickProtection: boolean | undefined;
-  idle: Record<string, number>;
-  idleOptions: { type?: string; interval?: unknown };
-}
+export function SettingsGeneral({ pushTab }: PageComponentProps) {
+  const dispatch = useAppDispatch();
+  const { t } = useTranslation();
 
-interface DispatchProps {
-  setIdle: (id: string) => void;
-}
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const idle = useAppSelector<Record<string, any>>(
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    state => (state.config as any)?.idle || {}
+  );
 
-type Props = PageComponentProps & WithTranslation & DispatchProps & StateProps;
+  const idleOptions = useAppSelector(state => state.idleOptions);
 
-class SettingsGeneralComponent extends React.Component<Props> {
-  langsHandler = () => this.props.pushTab(PAGES.LANGS_SETTINGS);
-  passwordHandler = () => this.props.pushTab(PAGES.CHANGE_PASSWORD);
-  setIdle = (id: string | number) => this.props.setIdle(id as string);
+  const selectList = Object.entries(idle)
+    .sort(([, a], [, b]) => a - b)
+    .map(([id, value]) => ({
+      id,
+      value,
+      text: t(`settings.time_${id}`, { defaultValue: id, key: id }),
+    }));
 
-  render() {
-    const { t, idle } = this.props;
+  return (
+    <div className={styles.content}>
+      <div className={`${styles.title1} title1`}>
+        {t('settings.settingsGeneral')}
+      </div>
 
-    const selectList = Object.entries(idle)
-      .sort(([, a], [, b]) => a - b)
-      .map(([id, value]) => ({
-        id,
-        value,
-        text: t(`settings.time_${id}`, { defaultValue: id, key: id }),
-      }));
-
-    return (
-      <div className={styles.content}>
-        <div className={`${styles.title1} title1`}>
-          {t('settings.settingsGeneral')}
+      <div className={styles.settingsMenu}>
+        <div className="margin-main-big">
+          <Select
+            description={t('settings.sessionTimeout')}
+            fill
+            selectList={selectList}
+            selected={idleOptions.type}
+            onSelectItem={(id: string | number) =>
+              dispatch(setIdle(id as string))
+            }
+          />
         </div>
 
-        <div className={styles.settingsMenu}>
-          <div className="margin-main-big">
-            <Select
-              description={t('settings.sessionTimeout')}
-              fill
-              selectList={selectList}
-              selected={this.props.idleOptions.type}
-              onSelectItem={this.setIdle}
-            />
-          </div>
-
-          <div className={cn(styles.settingsMenuItem, styles.password)}>
-            <Button
-              id="changePassword"
-              type="button"
-              view="transparent"
-              className={styles.settingsBtn}
-              onClick={this.passwordHandler}
-            >
-              <div className="body1 left">{t('settings.password')}</div>
-            </Button>
-          </div>
+        <div className={cn(styles.settingsMenuItem, styles.password)}>
+          <Button
+            id="changePassword"
+            type="button"
+            view="transparent"
+            className={styles.settingsBtn}
+            onClick={() => pushTab(PAGES.CHANGE_PASSWORD)}
+          >
+            <div className="body1 left">{t('settings.password')}</div>
+          </Button>
         </div>
       </div>
-    );
-  }
+    </div>
+  );
 }
-
-const mapStateToProps = function (store: AppState): StateProps {
-  return {
-    autoClickProtection: store.uiState && store.uiState.autoClickProtection,
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    idle: (store.config && (store.config as any).idle) || {},
-    idleOptions: store.idleOptions,
-  };
-};
-
-export const SettingsGeneral = connect(mapStateToProps, {
-  lock,
-  setUiState,
-  setIdle,
-})(withTranslation()(SettingsGeneralComponent));
