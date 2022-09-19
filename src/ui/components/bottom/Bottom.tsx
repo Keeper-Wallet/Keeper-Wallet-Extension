e6772e3f41dcefdb3595230c6292983ffd8d4b52
Sergey Slipchenko <faergeek@ya.ru>
2022-09-19 12:29:35 +0300

    Remove locked flag check in Bottom

--- src/ui/components/bottom/Bottom.tsx
+++ src/ui/components/bottom/Bottom.tsx
@@ -9,16 +9,13 @@ interface Props {
 
 export function Bottom({ noChangeNetwork }: Props) {
   const version = useAppSelector(state => state.version);
-  const isLocked = useAppSelector(
-    state => state.state?.locked || !state.state?.initialized
-  );
 
-  return !isLocked ? (
+  return (
     <div className={styles.bottom}>
       <Network noChangeNetwork={noChangeNetwork} />
       <div className="version basic500" data-testid="currentVersion">
         v {version}
       </div>
     </div>
-  ) : null;
+  );
 }
