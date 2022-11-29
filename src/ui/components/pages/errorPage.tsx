78c1a4c3ef12ad9f7d8737037f367adb933d0ad3
Sergey Slipchenko <faergeek@gmail.com>
2022-11-29 14:07:08 +0400

    Handle errors using react-router-dom

--- src/ui/components/pages/errorPage.tsx
+++ src/ui/components/pages/errorPage.tsx
@@ -1,22 +1,28 @@
+import { captureException } from '@sentry/react';
+import { useEffect } from 'react';
 import { useTranslation } from 'react-i18next';
+import { useRouteError } from 'react-router-dom';
 import { ExportButton, ResetButton } from 'ui/components/ui';
 
 import { HeadLogo } from '../head';
 import * as styles from './errorPage.module.css';
 
-interface Props {
-  error: Error;
-}
-
-export function ErrorPage({ error }: Props) {
+export function ErrorPage() {
+  const error = useRouteError();
   const { t } = useTranslation();
+
+  useEffect(() => {
+    captureException(error);
+  }, [error]);
 
   return (
     <div className={styles.wrapper}>
       <HeadLogo className={styles.logo} />
       <div className={styles.content}>
         <h2 className={styles.title}>{t('errorPage.title')}</h2>
-        <p className={styles.name}>{error.toString()}</p>
+        <p className={styles.name}>
+          {error instanceof Error ? error.message : String(error)}
+        </p>
       </div>
 
       <div className={styles.footer}>
