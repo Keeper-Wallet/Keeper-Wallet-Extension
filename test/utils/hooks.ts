f0ed6a59176ad0e296142e55f5f244a6d59c386a
Sergey Slipchenko <faergeek@gmail.com>
2022-12-01 08:50:06 +0400

    Start and stop testcontainers in parallel

--- test/utils/hooks.ts
+++ test/utils/hooks.ts
@@ -38,44 +38,45 @@ interface GlobalFixturesContext {
 export async function mochaGlobalSetup(this: GlobalFixturesContext) {
   const host = await new Network().start();
 
-  this.node = await new GenericContainer('wavesplatform/waves-private-node')
-    .withExposedPorts(6869)
-    .withNetworkMode(host.getName())
-    .withNetworkAliases('waves-private-node')
-    .start();
+  [this.node, this.selenium] = await Promise.all([
+    new GenericContainer('wavesplatform/waves-private-node')
+      .withExposedPorts(6869)
+      .withNetworkMode(host.getName())
+      .withNetworkAliases('waves-private-node')
+      .start(),
 
-  this.selenium = await new GenericContainer('selenium/standalone-chrome')
-    .withBindMount(
-      path.resolve(__dirname, '..', '..', 'dist'),
-      '/app/dist',
-      'ro'
-    )
-    .withBindMount(
-      path.resolve(__dirname, '..', 'fixtures'),
-      '/app/test/fixtures',
-      'ro'
-    )
-    .withExposedPorts(
-      {
-        container: 4444,
-        host: 4444,
-      },
-      {
-        container: 5900,
-        host: 5900,
-      },
-      {
-        container: 7900,
-        host: 7900,
-      }
-    )
-    .withNetworkMode(host.getName())
-    .start();
+    new GenericContainer('selenium/standalone-chrome')
+      .withBindMount(
+        path.resolve(__dirname, '..', '..', 'dist'),
+        '/app/dist',
+        'ro'
+      )
+      .withBindMount(
+        path.resolve(__dirname, '..', 'fixtures'),
+        '/app/test/fixtures',
+        'ro'
+      )
+      .withExposedPorts(
+        {
+          container: 4444,
+          host: 4444,
+        },
+        {
+          container: 5900,
+          host: 5900,
+        },
+        {
+          container: 7900,
+          host: 7900,
+        }
+      )
+      .withNetworkMode(host.getName())
+      .start(),
+  ]);
 }
 
 export async function mochaGlobalTeardown(this: GlobalFixturesContext) {
-  await this.selenium.stop();
-  await this.node.stop();
+  await Promise.all([this.selenium.stop(), this.node.stop()]);
 }
 
 export const mochaHooks = () => ({
