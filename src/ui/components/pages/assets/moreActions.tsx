ace6b4beeba83efef465c582039e6ff4a47f67ab
Sergey Slipchenko <faergeek@gmail.com>
2022-12-04 12:27:41 +0400

    Simplify MoreActions implementation

--- src/ui/components/pages/assets/moreActions.tsx
+++ src/ui/components/pages/assets/moreActions.tsx
@@ -1,23 +1,11 @@
-import { useState } from 'react';
-
 import * as styles from './moreActions.module.css';
 
 export function MoreActions({ children }: { children: React.ReactNode }) {
-  const [isExpanded, setIsExpanded] = useState(false);
-
   return (
-    <div
-      className={styles.moreActions}
-      onMouseLeave={() => setIsExpanded(false)}
-    >
-      {isExpanded && <div className={styles.content}>{children}</div>}
+    <div className={styles.root}>
+      <div className={styles.content}>{children}</div>
 
-      <button
-        className={styles.moreBtn}
-        type="button"
-        onMouseEnter={() => setIsExpanded(true)}
-        data-testid="moreBtn"
-      >
+      <div data-testid="moreBtn">
         <svg
           className={styles.moreIcon}
           height="28"
@@ -26,7 +14,7 @@ export function MoreActions({ children }: { children: React.ReactNode }) {
         >
           <path d="M7.00004 4.66665C7.64171 4.66665 8.16671 4.14165 8.16671 3.49998C8.16671 2.85831 7.64171 2.33331 7.00004 2.33331C6.35837 2.33331 5.83337 2.85831 5.83337 3.49998C5.83337 4.14165 6.35837 4.66665 7.00004 4.66665ZM7.00004 5.83331C6.35837 5.83331 5.83337 6.35831 5.83337 6.99998C5.83337 7.64165 6.35837 8.16665 7.00004 8.16665C7.64171 8.16665 8.16671 7.64165 8.16671 6.99998C8.16671 6.35831 7.64171 5.83331 7.00004 5.83331ZM7.00004 9.33331C6.35837 9.33331 5.83337 9.85831 5.83337 10.5C5.83337 11.1416 6.35837 11.6666 7.00004 11.6666C7.64171 11.6666 8.16671 11.1416 8.16671 10.5C8.16671 9.85831 7.64171 9.33331 7.00004 9.33331Z" />
         </svg>
-      </button>
+      </div>
     </div>
   );
 }
