c1783b7139af86d21f0f3fe25f4571a7bea95880
Denis Krasilnikov <dkrasilnikov@uniqsoft.ae>
2023-01-11 12:59:14 +0400

    Remove useless index.ts

--- test/utils/index.ts
+++
@@ -1,6 +0,0 @@
-import { Key, WebElement } from 'selenium-webdriver';
-
-export const clear = async (input: WebElement) => {
-  // some input controls are not cleared by input.clear()
-  await input.sendKeys(Key.HOME, Key.chord(Key.SHIFT, Key.END), Key.BACK_SPACE);
-};
