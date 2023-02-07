3b76d48ba4844cad7713abb39d99d68245fefa1a
Denis Krasilnikov <dkrasilnikov@uniqsoft.ae>
2023-02-07 18:20:31 +0400

    Remove @waves/waves-transactions

--- test/utils/nodeInteraction.ts
+++ test/utils/nodeInteraction.ts
@@ -1,21 +1,62 @@
-import { base58Decode } from '@keeper-wallet/waves-crypto';
-import { broadcast, transfer } from '@waves/waves-transactions';
+import {
+  base58Decode,
+  base58Encode,
+  createPrivateKey,
+  createPublicKey,
+  signBytes,
+  utf8Encode,
+} from '@keeper-wallet/waves-crypto';
+import { TRANSACTION_TYPE } from '@waves/ts-types';
 
+import { makeTxBytes, stringifyTransaction } from '../../src/messages/utils';
 import { DEFAULT_MINER_SEED } from './constants';
 
 export async function faucet({
   recipient,
   amount,
   nodeUrl,
+  chainId,
 }: {
   recipient: string;
   amount: number;
   nodeUrl: string;
+  chainId: number;
 }) {
-  return await broadcast(
-    transfer({ amount, recipient }, DEFAULT_MINER_SEED),
-    nodeUrl
+  const minerPrivateKeyBytes = await createPrivateKey(
+    utf8Encode(DEFAULT_MINER_SEED)
   );
+  const minerPublicKeyBytes = await createPublicKey(minerPrivateKeyBytes);
+  const minerPublicKey = base58Encode(minerPublicKeyBytes);
+
+  const data = {
+    type: TRANSACTION_TYPE.TRANSFER,
+    senderPublicKey: minerPublicKey,
+    recipient,
+    amount,
+    chainId,
+    version: 2 as const,
+    assetId: null,
+    feeAssetId: null,
+    fee: 100000,
+    timestamp: Date.now(),
+  };
+  const txBytes = makeTxBytes(data);
+  const signature = await signBytes(minerPrivateKeyBytes, txBytes);
+  const signedTx = {
+    ...data,
+    id: '',
+    initialFee: 0,
+    initialFeeAssetId: null,
+    proofs: [base58Encode(signature)],
+  };
+  await fetch(new URL('transactions/broadcast', nodeUrl), {
+    method: 'POST',
+    headers: {
+      accept: 'application/json; large-significand-format=string',
+      'content-type': 'application/json; charset=utf-8',
+    },
+    body: stringifyTransaction(signedTx),
+  });
 }
 
 export async function getNetworkByte(nodeUrl: string) {
