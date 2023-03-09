d0c4cb2d718607eaf70a3399b8b09962c52953b0
Timofei Domnikov <domnikov.timofei@gmail.com>
2023-03-09 11:23:44 +0300

    KEEP-1193: fix order signing with ledger account

--- src/wallets/getTxVersions.ts
+++ src/wallets/getTxVersions.ts
@@ -6,10 +6,9 @@ type TxVersions = {
   [T in MessageTx['type']]: Array<
     NonNullable<Extract<MessageTx, { type: T }>['version']>
   >;
-};
+} & Record<'1000' | '1001' | '1002' | '1003', number[]>;
 
-const DEFAULT_TX_VERSIONS: TxVersions &
-  Record<'1000' | '1001' | '1002' | '1003', number[]> = {
+const DEFAULT_TX_VERSIONS: TxVersions = {
   [TRANSACTION_TYPE.ISSUE]: [3, 2],
   [TRANSACTION_TYPE.TRANSFER]: [3, 2],
   [TRANSACTION_TYPE.REISSUE]: [3, 2],
@@ -45,6 +44,10 @@ const LEDGER_TX_VERSIONS: TxVersions = {
   [TRANSACTION_TYPE.SET_ASSET_SCRIPT]: [2, 1],
   [TRANSACTION_TYPE.INVOKE_SCRIPT]: [2, 1],
   [TRANSACTION_TYPE.UPDATE_ASSET_INFO]: [1],
+  '1000': [1],
+  '1001': [1],
+  '1002': [4, 3, 2, 1],
+  '1003': [1, 0],
 };
 
 export function getTxVersions(accountType: PreferencesAccount['type']) {
