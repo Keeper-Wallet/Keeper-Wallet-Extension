2b2ebf9487253fa87de9eb503d4a036df0c77da9
vlad-grigorian <vladimir.grigorian.1988@gmail.com>
2026-02-10 19:33:56 +0400

    KEEP-165, feat: setup Jest for unit testing

---
+++ jest.config.js
@@ -0,0 +1,45 @@
+export default {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  moduleDirectories: ['node_modules', '<rootDir>/src'],
+  testMatch: ['<rootDir>/src/**/*.test.ts'],
+  transform: {
+    '^.+\\.ts$': [
+      'ts-jest',
+      {
+        tsconfig: {
+          esModuleInterop: true,
+        },
+      },
+    ],
+  },
+
+  // Coverage config
+  collectCoverageFrom: [
+    'src/**/*.{ts,tsx}',
+    '!src/**/*.d.ts',
+    '!src/**/__tests__/**',
+    '!src/**/*.test.{ts,tsx}',
+    '!src/copied/**',
+    '!src/fonts/**',
+    '!src/assets/logos/**',
+    '!src/assets/unit0_tokens/**',
+    '!src/index.ejs',
+    '!src/background.ts',
+    '!src/contentscript.ts',
+    '!src/inpage.ts',
+  ],
+
+  coverageReporters: ['html', 'text', 'lcov', 'text-summary'],
+
+  coverageThreshold: {
+    global: {
+      statements: 0,
+      branches: 0,
+      functions: 0,
+      lines: 0,
+    },
+  },
+
+  coverageDirectory: 'coverage',
+};
